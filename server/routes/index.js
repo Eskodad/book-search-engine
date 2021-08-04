fda540f43e96650bb2292c826eb0caf981a8d762
Sahr Koroma <77806148+Eskodad@users.noreply.github.com>
2021-08-04 00:07:33 -0500

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
-import { join } from 'path';
-import apiRoutes from './api';
+const path = require('path');
+const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
 // serve up react front-end in production
 router.use((req, res) => {
-  res.sendFile(join(__dirname, '../../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../../client/build/index.html'));
 });
 
-export default router;
+module.exports = router;
