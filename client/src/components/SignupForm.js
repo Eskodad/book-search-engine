07459d55a37700aceeb8811e77e57507c095462d
Sahr Koroma <77806148+Eskodad@users.noreply.github.com>
2021-08-04 00:29:49 -0500

    Update SignupForm.js

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,20 +1,18 @@
 import React, { useState } from 'react';
-import { useMutation } from '@apollo/react-hooks';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-// import { createUser } from '../utils/API';
-import Auth from '../utils/auth';
+import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
 
 const SignupForm = () => {
   // set initial form state
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
+  const [addUser] = useMutation(ADD_USER);
   // set state for form validation
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
-
-  const[addUser] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -24,26 +22,27 @@ const SignupForm = () => {
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
-        // use try/catch instead of promises to handle errors
-      try {
-        // execute addUser mutation and pass in variable data from form
-        const { data } = await addUser({
-          variables: { ...userFormData}
-        });
-  
-        Auth.login(data.addUser.token)
-  
-      } catch (e) {
-        console.error(e);
-        setShowAlert(true);
-      }
-    
+    // check if form has everything (as per react-bootstrap docs)
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
 
-    setUserFormData({
-      username: '',
-      email: '',
-      password: '',
-    });
+    try {
+      const { data } = await addUser({
+		  variables: { ...userFormData }
+	  });
+
+      Auth.login(data.addUser.token);
+	  setUserFormData({
+		username: '',
+		email: '',
+		password: '',
+	  });
+    } catch (e) {
+      console.error(e);
+    }
   };
 
   return (
@@ -99,7 +98,6 @@ const SignupForm = () => {
           variant='success'>
           Submit
         </Button>
-        {/* {error && <div>Sign up failed</div>} */}
       </Form>
     </>
   );
