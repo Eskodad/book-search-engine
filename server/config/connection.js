903053aa4a08b0863c59a5792e9455fd69eace36
Sahr Koroma <77806148+Eskodad@users.noreply.github.com>
2021-08-04 00:16:06 -0500

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,13 +1,10 @@
-import { connect, connection } from 'mongoose';
+const mongoose = require('mongoose');
 
-connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/googlebooks',
-  {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
-export default connection;
+module.exports = mongoose.connection;
